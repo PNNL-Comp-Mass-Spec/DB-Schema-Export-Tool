58bd64dc7946ad54261913e588851e707070050a
alchemistmatt <matthew.monroe@pnnl.gov>
2017-04-12 16:38:52 -0700

    Update batch files

--- Automation/Synchronize_PRISM_DB_Schema_NoCommit.bat
+++ Automation/Synchronize_PRISM_DB_Schema_NoCommit.bat
@@ -4,10 +4,10 @@ rem Can auto-commit with /Commit
 F:
 cd "F:\My Documents\Projects\DataMining\Database_Tools\DB_Schema_Export_Tool\bin"
 
-DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:albert       /DB:mt_template_01                                                /sync:"F:\My Documents\Projects\DataMining\Database_Schema\MTS\MT_Template"  /Svn /Git /L /LogFolder:Logs /NoSubfolder
-DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:albert       /DB:pt_template_01                                                /sync:"F:\My Documents\Projects\DataMining\Database_Schema\MTS\PT_Template"  /Svn /Git /L /LogFolder:Logs /NoSubfolder
-DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Pogo         /DBList:mt_main,prism_rpt,prism_ifc,mts_master,MT_HistoricLog,dba /sync:"F:\My Documents\Projects\DataMining\Database_Schema\MTS"              /Svn /Git /L /LogFolder:Logs /Data:..\Automation\MTS_Data_Tables.txt
-DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:ProteinSeqs2 /DBList:Master_Sequences,Master_Seq_Scratch                       /sync:"F:\My Documents\Projects\DataMining\Database_Schema\MTS"              /Svn /Git /L /LogFolder:Logs
+DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:albert       /DB:mt_template_01                                                /sync:"F:\My Documents\Projects\DataMining\Database_Schema\MTS\MT_Template"  /Git /L /LogFolder:Logs /NoSubfolder
+DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:albert       /DB:pt_template_01                                                /sync:"F:\My Documents\Projects\DataMining\Database_Schema\MTS\PT_Template"  /Git /L /LogFolder:Logs /NoSubfolder
+DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Pogo         /DBList:mt_main,prism_rpt,prism_ifc,mts_master,MT_HistoricLog,dba /sync:"F:\My Documents\Projects\DataMining\Database_Schema\MTS"              /Git /L /LogFolder:Logs /Data:..\Automation\MTS_Data_Tables.txt
+DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:ProteinSeqs2 /DBList:Master_Sequences,Master_Seq_Scratch                       /sync:"F:\My Documents\Projects\DataMining\Database_Schema\MTS"              /Git /L /LogFolder:Logs
 
-DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Gigasax      /DBList:DMS_Capture,DMS_Data_Package,DMS_Pipeline,DMS5,Ontology_Lookup /sync:"F:\My Documents\Projects\DataMining\Database_Schema\DMS"         /Svn /Git /L /LogFolder:Logs /Data:..\Automation\DMS_Data_Tables.txt
-DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Proteinseqs  /DBList:Manager_Control,Protein_Sequences                              /sync:"F:\My Documents\Projects\DataMining\Database_Schema\DMS"         /Svn /Git /L /LogFolder:Logs /Data:..\Automation\ProteinSeqs_Data_Tables.txt
+DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Gigasax      /DBList:DMS_Capture,DMS_Data_Package,DMS_Pipeline,DMS5,Ontology_Lookup /sync:"F:\My Documents\Projects\DataMining\Database_Schema\DMS"         /Git /L /LogFolder:Logs /Data:..\Automation\DMS_Data_Tables.txt
+DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Proteinseqs  /DBList:Manager_Control,Protein_Sequences                              /sync:"F:\My Documents\Projects\DataMining\Database_Schema\DMS"         /Git /L /LogFolder:Logs /Data:..\Automation\ProteinSeqs_Data_Tables.txt
