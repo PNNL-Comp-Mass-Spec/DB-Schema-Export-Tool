f00acd6318eb95006b97489705f6367fea6bcd2f
alchemistmatt <matthew.monroe@pnnl.gov>
2017-11-03 15:18:18 -0700

    Stop tracking protein collection data

--- Automation/Synchronize_PRISM_DB_Schema_NoCommit.bat
+++ Automation/Synchronize_PRISM_DB_Schema_NoCommit.bat
@@ -5,7 +5,7 @@ F:
 cd "F:\My Documents\Projects\DataMining\Database_Tools\DB_Schema_Export_Tool\bin"
 
 DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Gigasax      /DBList:DMS_Capture,DMS_Data_Package,DMS_Pipeline,DMS5,Ontology_Lookup /sync:"F:\My Documents\Projects\DataMining\Database_Schema\DMS"         /Git /L /LogFolder:Logs /Data:..\Automation\DMS_Data_Tables.txt
-DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Proteinseqs  /DBList:Manager_Control,Protein_Sequences                              /sync:"F:\My Documents\Projects\DataMining\Database_Schema\DMS"         /Git /L /LogFolder:Logs /Data:..\Automation\ProteinSeqs_Data_Tables.txt
+DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Proteinseqs  /DBList:Manager_Control,Protein_Sequences                              /sync:"F:\My Documents\Projects\DataMining\Database_Schema\DMS"         /Git /L /LogFolder:Logs
 
 DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Pogo         /DB:mt_template_01                                                /sync:"F:\My Documents\Projects\DataMining\Database_Schema\MTS\MT_Template"  /Git /L /LogFolder:Logs /NoSubfolder
 DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Pogo         /DB:pt_template_01                                                /sync:"F:\My Documents\Projects\DataMining\Database_Schema\MTS\PT_Template"  /Git /L /LogFolder:Logs /NoSubfolder
