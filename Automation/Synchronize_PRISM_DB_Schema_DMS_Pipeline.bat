1b41bacea6bdb85f45c5ef6663c23fb2c9863e17
alchemistmatt <matthew.monroe@pnnl.gov>
2018-10-22 15:12:59 -0700

    Update batch files

--- Automation/Synchronize_PRISM_DB_Schema_DMS_Pipeline.bat
+++ Automation/Synchronize_PRISM_DB_Schema_DMS_Pipeline.bat
@@ -4,4 +4,4 @@ rem Can auto-commit with /Commit
 F:
 cd "F:\Documents\Projects\DataMining\Database_Tools\DB_Schema_Export_Tool\bin"
 
-DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Gigasax      /DBList:DMS_Pipeline /sync:"F:\Documents\Projects\DataMining\Database_Schema\DMS"         /Git /L /LogFolder:Logs /Data:..\Automation\DMS_Data_Tables.txt
+DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Gigasax      /DBList:DMS_Pipeline /sync:"F:\Documents\Projects\DataMining\Database_Schema\DMS"         /Git /L /LogDir:Logs /Data:..\Automation\DMS_Data_Tables.txt
