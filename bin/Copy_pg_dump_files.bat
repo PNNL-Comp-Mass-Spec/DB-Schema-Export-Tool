104d1a70aca059bc05c629abe0f84358181d9069
alchemistmatt <matthew.monroe@pnnl.gov>
2019-10-12 15:36:16 -0700

    Add batch file

---
+++ bin/Copy_pg_dump_files.bat
@@ -0,0 +1,13 @@
+xcopy "C:\Program Files\PostgreSQL\12\bin\pg_dumpall.exe" . /D
+xcopy "C:\Program Files\PostgreSQL\12\bin\pg_dump.exe" . /D
+xcopy "C:\Program Files\PostgreSQL\12\bin\libcurl.dll" . /D
+xcopy "C:\Program Files\PostgreSQL\12\bin\libiconv-2.dll" . /D
+xcopy "C:\Program Files\PostgreSQL\12\bin\libintl-8.dll" . /D
+xcopy "C:\Program Files\PostgreSQL\12\bin\libpq.dll" . /D
+xcopy "C:\Program Files\PostgreSQL\12\bin\libssl-1_1-x64.dll" . /D
+xcopy "C:\Program Files\PostgreSQL\12\bin\libwinpthread-1.dll" . /D
+xcopy "C:\Program Files\PostgreSQL\12\bin\libcrypto-1_1-x64.dll" . /D
+xcopy "C:\Program Files\PostgreSQL\12\bin\zlib1.dll" . /D
+
+pause
+
