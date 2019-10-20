7902297cf223318b960904f1f4bea978ad008d58
alchemistmatt <matthew.monroe@pnnl.gov>
2019-10-20 10:43:30 -0700

    Add new column map info class

---
+++ ColumnMapInfo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using PRISM;
+
+namespace DB_Schema_Export_Tool
+{
+    public class ColumnMapInfo
+    {
+        /// <summary>
+        /// Keys are source column name
+        /// Values are target column name
+        /// </summary>
+        private readonly Dictionary<string, string> mColumnNameMap;
+
+        public string SourceTableName { get; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="sourceTableName"></param>
+        public ColumnMapInfo(string sourceTableName)
+        {
+            SourceTableName = sourceTableName;
+            mColumnNameMap = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+        }
+
+        public void AddColumn(string sourceColumnName, string targetColumnName)
+        {
+            if (!mColumnNameMap.TryGetValue(sourceColumnName, out var existingTargetName))
+            {
+                mColumnNameMap.Add(sourceColumnName, targetColumnName);
+                return;
+            }
+
+            ConsoleMsgUtils.ShowDebug("Updating the target name for column {0} from {1} to {2} for table {3}",
+                                      sourceColumnName, existingTargetName, targetColumnName, SourceTableName);
+
+            mColumnNameMap[sourceColumnName] = targetColumnName;
+        }
+
+        public string GetTargetColumnName(string sourceColumnName)
+        {
+            if (!mColumnNameMap.TryGetValue(sourceColumnName, out var targetColumnName))
+                return sourceColumnName;
+
+            return targetColumnName;
+
+        }
+    }
+}
