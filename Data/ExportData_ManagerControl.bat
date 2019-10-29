19c991abbfdb36111404d26c481a0777dbe4a145
alchemistmatt <matthew.monroe@pnnl.gov>
2019-10-28 23:22:22 -0700

    Change the output directory

--- Data/ExportData_ManagerControl.bat
+++ Data/ExportData_ManagerControl.bat
@@ -1,2 +1,2 @@
-..\bin\DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Proteinseqs    /DBList:Manager_Control /data:..\Data\Manager_Control_Tables.tsv /map:..\Data\Manager_Control_Table_Columns.tsv /noschema /pgdump /pginsert /snakecase /ExportAllData /defaultschema:mc /ScriptLoad
+..\bin\DB_Schema_Export_Tool.exe F:\Cached_DBSchema_PgSQL /server:Proteinseqs    /DBList:Manager_Control /data:..\Data\Manager_Control_Tables.tsv /map:..\Data\Manager_Control_Table_Columns.tsv /noschema /pgdump /pginsert /snakecase /ExportAllData /defaultschema:mc /ScriptLoad
 pause
