24e40bec5a7855d39ec0f29939734a76ba58e858
alchemistmatt <matthew.monroe@pnnl.gov>
2020-01-03 19:48:06 -0800

    Update batch file that retrieves pg_dump.exe

--- bin/Copy_pg_dump_files.bat
+++ bin/Copy_pg_dump_files.bat
@@ -1,13 +1,13 @@
-xcopy "C:\Program Files\PostgreSQL\12\bin\pg_dumpall.exe" . /D
-xcopy "C:\Program Files\PostgreSQL\12\bin\pg_dump.exe" . /D
-xcopy "C:\Program Files\PostgreSQL\12\bin\libcurl.dll" . /D
-xcopy "C:\Program Files\PostgreSQL\12\bin\libiconv-2.dll" . /D
-xcopy "C:\Program Files\PostgreSQL\12\bin\libintl-8.dll" . /D
-xcopy "C:\Program Files\PostgreSQL\12\bin\libpq.dll" . /D
-xcopy "C:\Program Files\PostgreSQL\12\bin\libssl-1_1-x64.dll" . /D
-xcopy "C:\Program Files\PostgreSQL\12\bin\libwinpthread-1.dll" . /D
-xcopy "C:\Program Files\PostgreSQL\12\bin\libcrypto-1_1-x64.dll" . /D
-xcopy "C:\Program Files\PostgreSQL\12\bin\zlib1.dll" . /D
+xcopy "C:\Program Files\PostgreSQL\12\bin\pg_dumpall.exe" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\12\bin\pg_dump.exe" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\12\bin\libcurl.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\12\bin\libiconv-2.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\12\bin\libintl-8.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\12\bin\libpq.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\12\bin\libssl-1_1-x64.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\12\bin\libwinpthread-1.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\12\bin\libcrypto-1_1-x64.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\12\bin\zlib1.dll" . /D /Y
 
 pause
 
