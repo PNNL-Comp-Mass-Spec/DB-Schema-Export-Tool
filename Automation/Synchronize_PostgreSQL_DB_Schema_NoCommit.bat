f5c784dbd3207f6397c8e1a9bf610a5f2289a159
alchemistmatt <matthew.monroe@pnnl.gov>
2020-02-06 10:22:59 -0800

    Rename parameter file

--- Automation/Synchronize_PostgreSQL_DB_Schema_NoCommit.bat
+++ Automation/Synchronize_PostgreSQL_DB_Schema_NoCommit.bat
@@ -4,4 +4,4 @@ rem Can auto-commit with /Commit
 F:
 cd "F:\Documents\Projects\DataMining\Database_Tools\DB_Schema_Export_Tool\Automation"
 
-..\bin\DB_Schema_Export_Tool.exe /conf:PgSQL_ExportOptions_ManagerControl.conf
+..\bin\DB_Schema_Export_Tool.exe /conf:PgSQL_ExportOptions_Prismweb3.conf
