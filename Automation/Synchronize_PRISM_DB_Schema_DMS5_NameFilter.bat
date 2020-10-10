bfc80edf66160753b118ac98c9915a19bda71a2f
alchemistmatt <matthew.monroe@pnnl.gov>
2020-10-10 13:01:04 -0700

    Add batch file with argument /NameFilter

---
+++ Automation/Synchronize_PRISM_DB_Schema_DMS5_NameFilter.bat
@@ -0,0 +1,11 @@
+@echo off
+rem Can specify data tables for export using /data:F:\Cached_DBSchema\GigasaxTableData.txt 
+rem Can auto-commit with /Commit
+
+if [%1]==[] (Set NameFilter=AutoUpdate) Else (Set NameFilter=%1)
+
+F:
+cd "F:\Documents\Projects\DataMining\Database_Tools\DB_Schema_Export_Tool\bin"
+
+@echo on
+DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Gigasax      /DBList:DMS5 /NameFilter:%NameFilter% /sync:"F:\Documents\Projects\DataMining\Database_Schema\DMS"         /Git /L /LogDir:Logs /Data:..\Automation\DMS_Data_Tables.txt
