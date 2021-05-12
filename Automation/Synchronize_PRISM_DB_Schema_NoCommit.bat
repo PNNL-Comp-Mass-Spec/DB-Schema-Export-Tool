2ada7a4480f5dc8d2fffce3bd389a17385d6dbba
alchemistmatt <matthew.monroe@pnnl.gov>
2021-05-12 13:45:45 -0700

    Update server name in batch file

--- Automation/Synchronize_PRISM_DB_Schema_NoCommit.bat
+++ Automation/Synchronize_PRISM_DB_Schema_NoCommit.bat
@@ -12,6 +12,6 @@ DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Proteinseqs  /DBList:Manage
 DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Pogo         /DB:mt_template_01                                                /sync:"F:\Documents\Projects\DataMining\Database_Schema\MTS\MT_Template"  /Git /L /LogDir:Logs /NoSubdirectory
 DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Pogo         /DB:pt_template_01                                                /sync:"F:\Documents\Projects\DataMining\Database_Schema\MTS\PT_Template"  /Git /L /LogDir:Logs /NoSubdirectory
 DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Pogo         /DBList:mt_main,prism_rpt,prism_ifc,mts_master,MT_HistoricLog,dba /sync:"F:\Documents\Projects\DataMining\Database_Schema\MTS"              /Git /L /LogDir:Logs /Data:..\Automation\MTS_Data_Tables.txt
-DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:ProteinSeqs2 /DBList:Master_Sequences,Master_Seq_Scratch                       /sync:"F:\Documents\Projects\DataMining\Database_Schema\MTS"              /Git /L /LogDir:Logs
+DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Pogo         /DBList:Master_Sequences,Master_Seq_Scratch                       /sync:"F:\Documents\Projects\DataMining\Database_Schema\MTS"              /Git /L /LogDir:Logs
 
 popd
