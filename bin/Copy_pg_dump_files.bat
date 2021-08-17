4645a3d505c93843489392d339508fa7ee19deaf
alchemistmatt <matthew.monroe@pnnl.gov>
2021-08-17 13:17:19 -0700

    Update pg_dump to v13

--- bin/Copy_pg_dump_files.bat
+++ bin/Copy_pg_dump_files.bat
@@ -1,13 +1,13 @@
-xcopy "C:\Program Files\PostgreSQL\12\bin\pg_dumpall.exe" . /D /Y
-xcopy "C:\Program Files\PostgreSQL\12\bin\pg_dump.exe" . /D /Y
-xcopy "C:\Program Files\PostgreSQL\12\bin\libcurl.dll" . /D /Y
-xcopy "C:\Program Files\PostgreSQL\12\bin\libiconv-2.dll" . /D /Y
-xcopy "C:\Program Files\PostgreSQL\12\bin\libintl-8.dll" . /D /Y
-xcopy "C:\Program Files\PostgreSQL\12\bin\libpq.dll" . /D /Y
-xcopy "C:\Program Files\PostgreSQL\12\bin\libssl-1_1-x64.dll" . /D /Y
-xcopy "C:\Program Files\PostgreSQL\12\bin\libwinpthread-1.dll" . /D /Y
-xcopy "C:\Program Files\PostgreSQL\12\bin\libcrypto-1_1-x64.dll" . /D /Y
-xcopy "C:\Program Files\PostgreSQL\12\bin\zlib1.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\13\bin\pg_dumpall.exe" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\13\bin\pg_dump.exe" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\13\bin\libcurl.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\13\bin\libiconv-2.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\13\bin\libintl-8.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\13\bin\libpq.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\13\bin\libssl-1_1-x64.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\13\bin\libwinpthread-1.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\13\bin\libcrypto-1_1-x64.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\13\bin\zlib1.dll" . /D /Y
 
 pause
 
