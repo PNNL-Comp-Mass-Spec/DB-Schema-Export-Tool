acdb3bfeef983bfef43a670ae92669de8d669439
alchemistmatt <matthew.monroe@pnnl.gov>
2021-11-12 11:27:39 -0800

    Add batch file

---
+++ Automation/Synchronize_PRISM_DB_Schema_Ontology_Lookup.bat
@@ -0,0 +1,11 @@
+@echo off
+rem Can specify data tables for export using /data:F:\Cached_DBSchema\GigasaxTableData.txt 
+rem Can auto-commit with /Commit
+
+F:
+pushd "F:\Documents\Projects\DataMining\Database_Tools\DB_Schema_Export_Tool\bin"
+
+@echo on
+DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Gigasax      /DBList:Ontology_Lookup /sync:"F:\Documents\Projects\DataMining\Database_Schema\DMS"         /Git /L /LogDir:Logs /Data:..\Automation\DMS_Data_Tables.txt
+
+popd
