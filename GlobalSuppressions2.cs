f18154313f3a4decd5f2da4988849854646fe9af
alchemistmatt <matthew.monroe@pnnl.gov>
2022-04-08 17:44:41 -0700

    Suppress warnings and update program date

--- GlobalSuppressions2.cs
+++ GlobalSuppressions2.cs
@@ -6,3 +6,4 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Roslynator", "RCS1201:Use method chaining.", Justification = "Leave as-is for readability", Scope = "member", Target = "~M:DB_Schema_Export_Tool.frmMain.ShowAboutBox")]
+[assembly: SuppressMessage("Roslynator", "RCS1235:Optimize method call.", Justification = "Leave as-is since cannot implicitly convert from StringCollection to List<string>", Scope = "member", Target = "~M:DB_Schema_Export_Tool.DBSchemaExporterSQLServer.StringCollectionToList(System.Collections.Specialized.StringCollection)~System.Collections.Generic.IEnumerable{System.String}")]
