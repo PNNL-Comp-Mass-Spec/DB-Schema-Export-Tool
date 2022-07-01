4b24e6410eb4ba851ccac3b950588ff13c5df392
alchemistmatt <matthew.monroe@pnnl.gov>
2022-07-01 15:01:01 -0700

    Update example parameter files

--- Automation/Synchronize_PRISM_DB_Schema_DMS5_NameFilter.bat
+++ Automation/Synchronize_PRISM_DB_Schema_DMS5_NameFilter.bat
@@ -2,12 +2,15 @@
 rem Can specify data tables for export using /data:F:\Cached_DBSchema\GigasaxTableData.txt 
 rem Can auto-commit with /Commit
 
+rem Surround the name filter with double quotes if it is a comma-separated list
+
 if [%1]==[] (Set NameFilter=AutoUpdate) Else (Set NameFilter=%1)
 
 F:
 pushd "F:\Documents\Projects\DataMining\Database_Tools\DB_Schema_Export_Tool\bin"
 
 @echo on
+
 DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Gigasax      /DBList:DMS5 /NameFilter:%NameFilter% /sync:"F:\Documents\Projects\DataMining\Database_Schema\DMS"         /Git /L /LogDir:Logs /Data:..\Automation\DMS_Data_Tables.txt
 
 popd
