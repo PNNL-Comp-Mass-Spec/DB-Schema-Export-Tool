00889ecb42fba9841c16f489ee523c96439a3e1e
alchemistmatt <matthew.monroe@pnnl.gov>
2022-08-29 18:57:43 -0700

    Show optional export of schema from historic log databases

--- Automation/Synchronize_PRISM_DB_Schema_NoCommit.bat
+++ Automation/Synchronize_PRISM_DB_Schema_NoCommit.bat
@@ -7,6 +7,8 @@ pushd "F:\Documents\Projects\DataMining\Database_Tools\DB_Schema_Export_Tool\bin
 
 @echo on
 DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Gigasax      /DBList:DMS_Capture,DMS_Data_Package,DMS_Pipeline,DMS5,Ontology_Lookup,dba /sync:"F:\Documents\Projects\DataMining\Database_Schema\DMS"     /Git /L /LogDir:Logs /Data:..\Automation\DMS_Data_Tables.txt
+rem DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Gigasax  /DBList:DMSHistoricLog,DMSHistoricLogCapture,DMSHistoricLogPipeline        /sync:"F:\Documents\Projects\DataMining\Database_Schema\DMS"     /Git /L /LogDir:Logs /Data:..\Automation\DMS_Data_Tables.txt
+
 DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Proteinseqs  /DBList:Manager_Control,Protein_Sequences                                  /sync:"F:\Documents\Projects\DataMining\Database_Schema\DMS"     /Git /L /LogDir:Logs
 
 DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Pogo         /DB:mt_template_01                                                /sync:"F:\Documents\Projects\DataMining\Database_Schema\MTS\MT_Template"  /Git /L /LogDir:Logs /NoSubdirectory
