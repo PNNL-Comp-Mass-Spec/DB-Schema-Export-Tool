6227ce139de463aa435db9fec733793fa9fb8dd0
alchemistmatt <matthew.monroe@pnnl.gov>
2022-12-22 21:08:54 -0800

    Update program date and cached DLL

--- SchemaExportOptions.cs
+++ SchemaExportOptions.cs
@@ -17,7 +17,7 @@ namespace DB_Schema_Export_Tool
         /// <summary>
         /// Program date
         /// </summary>
-        public const string PROGRAM_DATE = "August 21, 2022";
+        public const string PROGRAM_DATE = "December 22, 2022";
 
         /// <summary>
         /// Default output directory name prefix
