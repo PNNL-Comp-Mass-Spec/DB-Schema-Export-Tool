66acc2974dc79717a94134dec641395f6894c4c9
alchemistmatt <matthew.monroe@pnnl.gov>
2023-02-15 17:10:25 -0800

    Use separate cache directories to avoid merging master DB files

--- Automation/Synchronize_SQL_Server_Settings_Logins_and_Jobs.bat
+++ Automation/Synchronize_SQL_Server_Settings_Logins_and_Jobs.bat
@@ -4,22 +4,22 @@ F:
 pushd "F:\Documents\Projects\DataMining\Database_Tools\DB_Schema_Export_Tool\bin"
 
 @echo on
-DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Gigasax      /ServerInfo /DBList:master /sync:"F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Gigasax"     /Git /L /LogDir:Logs
-DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Proteinseqs  /ServerInfo /DBList:master /sync:"F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\ProteinSeqs" /Git /L /LogDir:Logs
-DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Pogo         /ServerInfo /DBList:master /sync:"F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Pogo"        /Git /L /LogDir:Logs
+DB_Schema_Export_Tool.exe F:\Cached_DBSchema\Gigasax     /server:Gigasax      /ServerInfo /DBList:master /sync:"F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Gigasax"     /Git /L /LogDir:Logs
+DB_Schema_Export_Tool.exe F:\Cached_DBSchema\ProteinSeqs /server:Proteinseqs  /ServerInfo /DBList:master /sync:"F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\ProteinSeqs" /Git /L /LogDir:Logs
+DB_Schema_Export_Tool.exe F:\Cached_DBSchema\Pogo        /server:Pogo         /ServerInfo /DBList:master /sync:"F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Pogo"        /Git /L /LogDir:Logs
 
 popd
 
 @echo off
 echo.
 echo Use Beyond Compare to synchronize the following directories
-echo "F:\Cached_DBSchema\ServerSchema__Gigasax       with  F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Gigasax"
-echo "F:\Cached_DBSchema\ServerSchema__Proteinseqs   with  F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Proteinseqs"
-echo "F:\Cached_DBSchema\ServerSchema__Pogo          with  F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Pogo"
+echo "F:\Cached_DBSchema\Gigasax\ServerSchema__Gigasax          with  F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Gigasax"
+echo "F:\Cached_DBSchema\ProteinSeqs\ServerSchema__Proteinseqs  with  F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Proteinseqs"
+echo "F:\Cached_DBSchema\Pogo\ServerSchema__Pogo                with  F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Pogo"
 
-"C:\Program Files\Beyond Compare 4\BCompare.exe" "F:\Cached_DBSchema\ServerSchema__Gigasax"     "F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Gigasax"
-"C:\Program Files\Beyond Compare 4\BCompare.exe" "F:\Cached_DBSchema\ServerSchema__Proteinseqs" "F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Proteinseqs"
-"C:\Program Files\Beyond Compare 4\BCompare.exe" "F:\Cached_DBSchema\ServerSchema__Pogo"        "F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Pogo"
+"C:\Program Files\Beyond Compare 4\BCompare.exe" "F:\Cached_DBSchema\Gigasax\ServerSchema__Gigasax"         "F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Gigasax"
+"C:\Program Files\Beyond Compare 4\BCompare.exe" "F:\Cached_DBSchema\ProteinSeqs\ServerSchema__Proteinseqs" "F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Proteinseqs"
+"C:\Program Files\Beyond Compare 4\BCompare.exe" "F:\Cached_DBSchema\Pogo\ServerSchema__Pogo"               "F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Pogo"
 
 echo.
 echo "For PostgreSQL, run Synchronize_PostgreSQL_DB_Schema_PrismDB1.bat then synchronize these directories"
