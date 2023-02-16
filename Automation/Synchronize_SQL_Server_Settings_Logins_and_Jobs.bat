ac707c72cf804b722c7b6b98684bb50afa5d6c58
alchemistmatt <matthew.monroe@pnnl.gov>
2023-02-15 16:04:55 -0800

    Add batch file for exporting SQL Server settings, including logins and SQL Server Agent jobs

---
+++ Automation/Synchronize_SQL_Server_Settings_Logins_and_Jobs.bat
@@ -0,0 +1,24 @@
+@echo off
+
+F:
+pushd "F:\Documents\Projects\DataMining\Database_Tools\DB_Schema_Export_Tool\bin"
+
+@echo on
+DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Gigasax      /ServerInfo /DBList:master /sync:"F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Gigasax"     /Git /L /LogDir:Logs
+DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Proteinseqs  /ServerInfo /DBList:master /sync:"F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\ProteinSeqs" /Git /L /LogDir:Logs
+
+DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Pogo         /ServerInfo /DBList:master /sync:"F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Pogo"        /Git /L /LogDir:Logs
+popd
+
+@echo off
+echo.
+echo Use Beyond Compare to synchronize the following directories
+echo "F:\Cached_DBSchema\ServerSchema__Gigasax       with  F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Gigasax"
+echo "F:\Cached_DBSchema\ServerSchema__Proteinseqs   with  F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Proteinseqs"
+echo "F:\Cached_DBSchema\ServerSchema__Pogo          with  F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Pogo"
+
+"C:\Program Files\Beyond Compare 4\BCompare.exe" "F:\Cached_DBSchema\ServerSchema__Gigasax"     "F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Gigasax"
+"C:\Program Files\Beyond Compare 4\BCompare.exe" "F:\Cached_DBSchema\ServerSchema__Proteinseqs" "F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Proteinseqs"
+"C:\Program Files\Beyond Compare 4\BCompare.exe" "F:\Cached_DBSchema\ServerSchema__Pogo"        "F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Pogo"
+
+pause
