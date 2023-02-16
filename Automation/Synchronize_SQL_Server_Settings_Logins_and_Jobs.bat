740c308f6f7dc11dc1c066b9ab1e06b9b555e168
alchemistmatt <matthew.monroe@pnnl.gov>
2023-02-15 16:38:54 -0800

    Add PostgreSQL info to batch file

--- Automation/Synchronize_SQL_Server_Settings_Logins_and_Jobs.bat
+++ Automation/Synchronize_SQL_Server_Settings_Logins_and_Jobs.bat
@@ -6,8 +6,8 @@ pushd "F:\Documents\Projects\DataMining\Database_Tools\DB_Schema_Export_Tool\bin
 @echo on
 DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Gigasax      /ServerInfo /DBList:master /sync:"F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Gigasax"     /Git /L /LogDir:Logs
 DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Proteinseqs  /ServerInfo /DBList:master /sync:"F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\ProteinSeqs" /Git /L /LogDir:Logs
-
 DB_Schema_Export_Tool.exe F:\Cached_DBSchema /server:Pogo         /ServerInfo /DBList:master /sync:"F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Pogo"        /Git /L /LogDir:Logs
+
 popd
 
 @echo off
@@ -20,5 +20,12 @@ echo "F:\Cached_DBSchema\ServerSchema__Pogo          with  F:\Documents\Projects
 "C:\Program Files\Beyond Compare 4\BCompare.exe" "F:\Cached_DBSchema\ServerSchema__Gigasax"     "F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Gigasax"
 "C:\Program Files\Beyond Compare 4\BCompare.exe" "F:\Cached_DBSchema\ServerSchema__Proteinseqs" "F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Proteinseqs"
 "C:\Program Files\Beyond Compare 4\BCompare.exe" "F:\Cached_DBSchema\ServerSchema__Pogo"        "F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Pogo"
+
+echo.
+echo "For PostgreSQL, run Synchronize_PostgreSQL_DB_Schema_PrismDB1.bat then synchronize these directories"
+echo "F:\Documents\Projects\DataMining\Database_Schema\DMS_PgSQL\dms"
+echo "F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\PrismDB1\DMS_PgSQL\dms"
+echo.
+echo "In Beyond Compare, filter using: _extension*;_server*;_user*"
 
 pause
