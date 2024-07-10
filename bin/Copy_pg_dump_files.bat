83f89437e975b282c899813e0852e6bb12df18cb
alchemistmatt <matthew.monroe@pnnl.gov>
2024-07-10 16:48:26 -0700

    Update batch file that copies pg_dump executables and DLLs

--- bin/Copy_pg_dump_files.bat
+++ bin/Copy_pg_dump_files.bat
@@ -1,13 +1,19 @@
-xcopy "C:\Program Files\PostgreSQL\14\bin\pg_dumpall.exe" . /D /Y
-xcopy "C:\Program Files\PostgreSQL\14\bin\pg_dump.exe" . /D /Y
-xcopy "C:\Program Files\PostgreSQL\14\bin\libcurl.dll" . /D /Y
-xcopy "C:\Program Files\PostgreSQL\14\bin\libiconv-2.dll" . /D /Y
-xcopy "C:\Program Files\PostgreSQL\14\bin\libintl-8.dll" . /D /Y
-xcopy "C:\Program Files\PostgreSQL\14\bin\libpq.dll" . /D /Y
-xcopy "C:\Program Files\PostgreSQL\14\bin\libssl-1_1-x64.dll" . /D /Y
-xcopy "C:\Program Files\PostgreSQL\14\bin\libwinpthread-1.dll" . /D /Y
-xcopy "C:\Program Files\PostgreSQL\14\bin\libcrypto-1_1-x64.dll" . /D /Y
-xcopy "C:\Program Files\PostgreSQL\14\bin\zlib1.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\16\bin\pg_dumpall.exe" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\16\bin\pg_dump.exe" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\16\bin\libcrypto-3-x64.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\16\bin\libecpg.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\16\bin\libecpg_compat.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\16\bin\libiconv-2.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\16\bin\libintl-9.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\16\bin\liblz4.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\16\bin\libpgtypes.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\16\bin\libpq.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\16\bin\libssl-3-x64.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\16\bin\libwinpthread-1.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\16\bin\libxml2.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\16\bin\libxslt.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\16\bin\libzstd.dll" . /D /Y
+xcopy "C:\Program Files\PostgreSQL\16\bin\zlib1.dll" . /D /Y
 
 pause
 
