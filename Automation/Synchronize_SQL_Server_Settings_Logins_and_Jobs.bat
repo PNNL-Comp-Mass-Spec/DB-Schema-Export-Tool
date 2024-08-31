5135a1ebecaf1f40f902920820f7ad169b1e2829
alchemistmatt <matthew.monroe@pnnl.gov>
2024-08-30 21:03:49 -0700

    Reference Beyond Compare 5 instead of BC4

--- Automation/Synchronize_SQL_Server_Settings_Logins_and_Jobs.bat
+++ Automation/Synchronize_SQL_Server_Settings_Logins_and_Jobs.bat
@@ -17,9 +17,9 @@ echo "F:\Cached_DBSchema\Gigasax\ServerSchema__Gigasax          with  F:\Documen
 echo "F:\Cached_DBSchema\ProteinSeqs\ServerSchema__Proteinseqs  with  F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Proteinseqs"
 echo "F:\Cached_DBSchema\Pogo\ServerSchema__Pogo                with  F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Pogo"
 
-"C:\Program Files\Beyond Compare 4\BCompare.exe" "F:\Cached_DBSchema\Gigasax\ServerSchema__Gigasax"         "F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Gigasax"
-"C:\Program Files\Beyond Compare 4\BCompare.exe" "F:\Cached_DBSchema\ProteinSeqs\ServerSchema__Proteinseqs" "F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Proteinseqs"
-"C:\Program Files\Beyond Compare 4\BCompare.exe" "F:\Cached_DBSchema\Pogo\ServerSchema__Pogo"               "F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Pogo"
+"C:\Program Files\Beyond Compare 5\BCompare.exe" "F:\Cached_DBSchema\Gigasax\ServerSchema__Gigasax"         "F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Gigasax"
+"C:\Program Files\Beyond Compare 5\BCompare.exe" "F:\Cached_DBSchema\ProteinSeqs\ServerSchema__Proteinseqs" "F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Proteinseqs"
+"C:\Program Files\Beyond Compare 5\BCompare.exe" "F:\Cached_DBSchema\Pogo\ServerSchema__Pogo"               "F:\Documents\Projects\DataMining\Database_Schema\Server_Config_Info\Pogo"
 
 echo.
 echo "For PostgreSQL, run Synchronize_PostgreSQL_DB_Schema_PrismDB1.bat then synchronize these directories"
