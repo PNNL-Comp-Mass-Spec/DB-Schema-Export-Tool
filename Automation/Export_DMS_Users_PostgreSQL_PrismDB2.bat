c28b155f50d85aa4136fdd087a8afea625aae736
alchemistmatt <matthew.monroe@pnnl.gov>
2026-01-14 17:24:09 -0800

    Add batch files and config files for exporting t_users table data from DMS

---
+++ Automation/Export_DMS_Users_PostgreSQL_PrismDB2.bat
@@ -0,0 +1,13 @@
+@echo off
+rem Batch file for exporting DDL for tables, views, functions, procedures, etc.
+rem
+rem DB_Schema_Export_Tool.exe can be found at https://ci.appveyor.com/project/PNNLCompMassSpec/db-schema-export-tool/build/artifacts
+rem PgSQL_ExportUsers_PrismDB2.conf can be found at https://github.com/PNNL-Comp-Mass-Spec/DB-Schema-Export-Tool/blob/master/Automation/PgSQL_ExportUsers_PrismDB2.conf
+
+F:
+pushd "F:\Documents\Projects\DataMining\Database_Tools\DB_Schema_Export_Tool\Automation"
+
+@echo on
+..\bin\DB_Schema_Export_Tool.exe /conf:PgSQL_ExportUsers_PrismDB2.conf
+
+popd
